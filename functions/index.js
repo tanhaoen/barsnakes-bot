95f8cb6a1852c14a6c624aa306f0a858344019b7
tanhaoen <nathanieltan1701@gmail.com>
2023-06-03 17:27:39 +0800

    Removed the read_sample_data function

--- functions/index.js
+++ functions/index.js
@@ -1,60 +1,51 @@
 const functions = require("firebase-functions");
 const { Telegraf } = require("telegraf");
 const cron = require("node-cron");
+const fs = require("fs");
 
-const { AccuWeather, read_sample_data } = require("./modules/weather_api.js");
+const { AccuWeather } = require("./modules/weather_api.js");
 
 const weatherAPI = new AccuWeather(functions.config().accuweather.apikey);
-// const sampleData = read_sample_data();
 
 const bot = new Telegraf(functions.config().telegram.token, {
     telegram: { webhookReply: true },
 })
 
-// // error handling
-// bot.catch((err, ctx) => {
-// 	functions.logger.error("[Bot] Error", err);
-// 	return ctx.reply(`Ooops, encountered an error for ${ctx.updateType}`, err);
-// })
-
-// // initialize the commands
-// bot.command("/start", (ctx) => ctx.reply("Hello! Send any message and I will copy it."));
-// // copy every message and send to the user
-// bot.on("message", (ctx) => ctx.telegram.sendCopy(ctx.chat.id, ctx.message));
-
-// // handle all telegram updates with HTTPs trigger
-// exports.echoBot = functions.https.onRequest(async (request, response) => {
-// 	functions.logger.log("Incoming message", request.body)
-// 	return await bot.handleUpdate(request.body, response).then((rv) => {
-// 		// if it"s not a request from the telegram, rv will be undefined, but we should respond with 200
-// 		return !rv && response.sendStatus(200);
-// 	})
-// })
-
 bot.start((ctx) => {ctx.reply("Welcome!")});
-
-// bot.start((ctx) => ctx.reply("Welcome!"));
-// bot.hears('hi', (ctx) => ctx.reply('Hey there!'))
 
 let weatherJob = null;
 
-bot.command("startWeather", (ctx) => {
-    const chatId = ctx.chat.id;
-    console.log("Starting weekly reminders");
-    ctx.reply("Starting weekly reminders on Tuesday at 4pm");
-    weatherJob = cron.schedule("0 16 * * TUE", () => {
-        bot.telegram.sendMessage(chatId, "Hello! It's time to submit your weekly report!");
+bot.command("startweather", async (ctx) => {
+    ctx.reply("Starting CRON job for every minute");
+    // const chatId = ctx.chat.id;
+    // console.log("Starting weekly reminders");
+    // Read from AccuWeather API
+    // const data = await weatherAPI.getCurrentConditions();
+    // bot.telegram.sendMessage(-817325123, data[0].WeatherText);
+
+    // Read sample data from sample.json (for testing to avoid exceeding API call limit)
+    const jsonString = fs.readFileSync("modules/sample.json", "utf8");
+    const data = JSON.parse(jsonString);
+
+    // Setup test cron job to send every minute
+    weatherJob = cron.schedule("* * * * *", () => {
+        bot.telegram.sendMessage(-817325123, data.WeatherText);
     });
+
+    // Setup cron job to send every Tuesday at 5:30pm
+    // weatherJob = cron.schedule("30 17 * * TUE", () => {
+    //     console.log("Sending weekly reminder");
+    //     bot.telegram.sendMessage(chatId, data.WeatherText);
+    // });
 });
 
 
-bot.command("stopWeather", (ctx) => {
-    const chatId = ctx.chat.id;
+bot.command("stopweather", (ctx) => {
     if (weatherJob) {
         weatherJob.stop();
-        ctx.reply("Weekly reminders stopped");
+        ctx.reply("Stopping CRON job");
     } else {
-        ctx.reply("Weekly reminders not started");
+        ctx.reply("CRON job not started");
     }
 });
 
--- functions/modules/weather_api.js
+++ functions/modules/weather_api.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 class AccuWeather {
     // Read from AccuWeather API
     constructor(apiKey) {
@@ -5,7 +7,7 @@ class AccuWeather {
         this.locationKey = "1569686";
     }
     
-    getCurrentConditions() {
+    async getCurrentConditions() {
         // Setup query params
         const params = new URLSearchParams({
             apikey: this.apiKey,
@@ -14,7 +16,6 @@ class AccuWeather {
         return fetch(`http://dataservice.accuweather.com/currentconditions/v1/${this.locationKey}?${params}`)
             .then(response => response.json())
             .then(data => {
-                console.log(data);
                 return data;
             })
             .catch(error => console.log(error));
@@ -23,19 +24,4 @@ class AccuWeather {
 
 // ---------------------------------------------
 
-const read_sample_data = () => {
-    // Read sample data from sample.json
-    const fs = require('fs');
-    fs.readFile('sample.json', 'utf8', (err, jsonString) => {
-        if (err) {
-            console.log("File read failed:", err);
-            return;
-        }
-        console.log('File data:', jsonString);
-        const data = JSON.parse(jsonString);
-
-        return data;
-    });
-};
-
-module.exports = { AccuWeather, read_sample_data };
+module.exports = { AccuWeather };
