e43554d6ab18ac1a6530334766ca99436c2baf35
tanhaoen <nathanieltan1701@gmail.com>
2023-06-05 23:15:38 +0800

    Modularized weather and attendance functions

--- functions/index.js
+++ functions/index.js
@@ -3,9 +3,10 @@ const { Telegraf } = require("telegraf");
 const cron = require("node-cron");
 const fs = require("fs");
 
-const { AccuWeather } = require("./modules/weather_api.js");
+const { startAttendancePolls, stopAttendancePolls } = require('./attendanceJobs');
+const { startWeatherJobs, stopWeatherJobs } = require('./weatherJobs');
 
-const weatherAPI = new AccuWeather(process.env.ACCUWEATHER_API_KEY);
+
 const chatId = -817325123;
 
 const bot = new Telegraf(process.env.BOT_TOKEN, {
@@ -14,121 +15,24 @@ const bot = new Telegraf(process.env.BOT_TOKEN, {
 
 bot.start((ctx) => {ctx.reply("Welcome!")});
 
-let weatherJobs = [];
 
 bot.command("startweather", async (ctx) => {
-    ctx.reply("Starting scheduled weather reports");
-    console.log("Starting scheduled weather report");
-    // Read sample data from sample.json (for testing to avoid exceeding API call limit)
-    // const jsonString = fs.readFileSync("modules/sample.json", "utf8");
-    // const data = JSON.parse(jsonString);
-
-    const weatherJobTimings = [
-        // "* * * * *",
-        "0 17 * * TUE",
-        "0 18 * * TUE",
-        "0 9 * * SAT",
-        "0 10 * * SAT"
-    ];
-
-    weatherJobTimings.forEach((timing) => {
-        weatherJobs.push(cron.schedule(timing, async () => {
-            // Read from AccuWeather API
-            let data = await weatherAPI.getCurrentConditions();
-            data = data[0];
-
-            let precip1Hr = data.Precip1hr.Metric.Value;
-            let precip1HrStatus = null;
-
-            if (precip1Hr < 0.1) {
-                precip1HrStatus = false;
-            } else if (precip1Hr <= 2.5) {
-                precip1HrStatus = "light rain";
-            } else if (precip1Hr <= 7.6) {
-                precip1HrStatus = "moderate rain";
-            } else {
-                precip1HrStatus = "heavy rain";
-            }
-
-            if (data.HasPrecipitation && precip1Hr) { 
-                bot.telegram.sendMessage(chatId, `Currently raining at Serangoon, with ${precip1HrStatus} over the past hour`);
-            } else if (data.HasPrecipitation && !precip1Hr) {
-                bot.telegram.sendMessage(chatId, `Currently raining at Serangoon`);
-            } else if (!data.HasPrecipitation && precip1Hr) {
-                bot.telegram.sendMessage(chatId, `Currently not raining at Serangoon, but ${precip1HrStatus} over the past hour. Floor may be wet!`);
-            } else {
-                bot.telegram.sendMessage(chatId, `All clear at Serangoon!`);
-            }
-        }));
-    });
+    startWeatherJobs(bot, chatId, ctx);
 });
 
 
 bot.command("stopweather", (ctx) => {
-    if (weatherJobs) {
-        weatherJobs.forEach((job) => {
-            job.stop();
-        });
-        console.log("Stopping scheduled weather report");
-        ctx.reply("Stopping scheduled weather reports");
-    } else {
-        console.log("Scheduled weather reports not started");
-        ctx.reply("Scheduled weather reports not started");
-    }
+    stopWeatherJobs(ctx);
 });
 
 
-let tueAttendanceJob;
-let satAttendanceJob;
-let testAttendanceJob;
-
-bot.command("startattendance", (ctx) => {
-    tueAttendanceJob = cron.schedule("0 18 * * MON", () => {
-        const pollQuestion = "Tues Workout";
-        const pollOptions = ["Yes@Tues", "Yes@Wed", "OTOT", "Busy"];
-
-        bot.telegram.sendPoll(-817325123, pollQuestion, pollOptions, {
-            is_anonymous: false
-        });
-    });
-
-    satAttendanceJob = cron.schedule("0 18 * * FRI", () => {
-        const pollQuestion = "Sat Workout";
-        const pollOptions = ["Yes", "OTOT", "Busy"];
-
-        bot.telegram.sendPoll(-817325123, pollQuestion, pollOptions, {
-            is_anonymous: false
-        });
-    });
-
-    // testAttendanceJob = cron.schedule("* * * * *", () => {
-    //     const pollQuestion = "Test Poll";
-    //     const pollOptions = ["Yes", "No"];
-
-    //     bot.telegram.sendPoll(-817325123, pollQuestion, pollOptions, {
-    //         is_anonymous: false
-    //     });
-    // }
-    // );
+bot.command('startattendance', (ctx) => {
+    startAttendancePolls(bot, chatId, ctx);
 });
 
 
-bot.command("stopattendance", (ctx) => {
-    if (tueAttendanceJob) {
-        tueAttendanceJob.stop();
-        ctx.reply("Stopping Tuesday attendance poll");
-    }
-    if (satAttendanceJob) {
-        satAttendanceJob.stop();
-        ctx.reply("Stopping Saturday attendance poll");
-    }
-    if (testAttendanceJob) {
-        testAttendanceJob.stop();
-        ctx.reply("Stopping test attendance poll");
-    }
-    if (!tueAttendanceJob && !satAttendanceJob) {
-        ctx.reply("No attendance polls running");
-    }
+bot.command('stopattendance', (ctx) => {
+    stopAttendancePolls(ctx);
 });
 
 
@@ -137,7 +41,7 @@ bot.command("feedback", (ctx) => {
     bot.on("text", (ctx) => {
         const { text } = ctx.message;
         ctx.reply("Thank you for your feedback!");
-        bot.telegram.sendMessage('tanhaoen', text);
+        bot.telegram.sendMessage(-817325123, text);
     });
 });
 
---
+++ functions/weatherJobs.js
@@ -0,0 +1,86 @@
+const cron = require('node-cron');
+const { AccuWeather } = require("./modules/weather_api.js");
+
+const weatherAPI = new AccuWeather(process.env.ACCUWEATHER_API_KEY);
+
+let weatherJobs;
+let isJobsStarted;
+
+function startWeatherJobs(bot, chatId, ctx) {
+    if (isJobsStarted) {
+        console.log('Weather jobs already started');
+        ctx.reply('Weather jobs already started');
+        return;
+    }
+
+    weatherJobs = [];
+
+    console.log("Starting scheduled weather report");
+    ctx.reply("Starting scheduled weather report");
+    // Read sample data from sample.json (for testing to avoid exceeding API call limit)
+    // const jsonString = fs.readFileSync("modules/sample.json", "utf8");
+    // const data = JSON.parse(jsonString);
+
+    const weatherJobTimings = [
+        // "* * * * *",
+        "0 17 * * TUE",
+        "0 18 * * TUE",
+        "0 9 * * SAT",
+        "0 10 * * SAT"
+    ];
+
+    weatherJobTimings.forEach((timing) => {
+        weatherJobs.push(cron.schedule(timing, async () => {
+            // Read from AccuWeather API
+            let data = await weatherAPI.getCurrentConditions();
+            console.log('Weather job is triggered');
+            data = data[0];
+            // console.log(data);
+
+            let precip1Hr = data.Precip1hr.Metric.Value;
+            let precip1HrStatus = null;
+
+            if (precip1Hr < 0.1) {
+                precip1HrStatus = false;
+            } else if (precip1Hr <= 2.5) {
+                precip1HrStatus = "light rain";
+            } else if (precip1Hr <= 7.6) {
+                precip1HrStatus = "moderate rain";
+            } else {
+                precip1HrStatus = "heavy rain";
+            }
+
+            if (data.HasPrecipitation && precip1Hr) { 
+                bot.telegram.sendMessage(chatId, `Currently raining at Serangoon, with ${precip1HrStatus} over the past hour`);
+            } else if (data.HasPrecipitation && !precip1Hr) {
+                bot.telegram.sendMessage(chatId, `Currently raining at Serangoon`);
+            } else if (!data.HasPrecipitation && precip1Hr) {
+                bot.telegram.sendMessage(chatId, `Currently not raining at Serangoon, but ${precip1HrStatus} over the past hour. Floor may be wet!`);
+            } else {
+                bot.telegram.sendMessage(chatId, `All clear at Serangoon!`);
+            }
+        }));
+    });
+
+    isJobsStarted = true;
+}
+
+
+function stopWeatherJobs (ctx) {
+    if (isJobsStarted) {
+        weatherJobs.forEach((job) => {
+            job.stop();
+        });
+        console.log("Stopping scheduled weather report");
+        ctx.reply("Stopping scheduled weather report");
+    } else {
+        console.log("Scheduled weather reports not started");
+        ctx.reply("Scheduled weather reports not started");
+        return;
+    }
+
+    weatherJobs = []; // Clear the weatherJobs array
+    isJobsStarted = false;
+}
+
+module.exports = { startWeatherJobs, stopWeatherJobs };
